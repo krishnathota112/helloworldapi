b5336ce9c8fdf2f6817b9ae880b578a9d00b0b81
krishnathota112 <145286478+krishnathota112@users.noreply.github.com>
2025-09-10 22:56:12 +0530

    Update settings.py

--- hello_api/settings.py
+++ hello_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-iu=x36doyi%5@y!i&d(*i0(^@_h!%f97z)t3(!2vq+%)5u4hay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
